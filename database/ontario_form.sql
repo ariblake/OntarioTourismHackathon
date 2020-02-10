c23f555886b4629570f3e7d5c8cd6547a54615a1
unscientifi <a_sun69708@fanshaweonline.ca>
2020-02-10 11:26:04 -0500

    new database

--- database/ontario_form.sql
+++ database/ontario_form.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Feb 10, 2020 at 03:42 PM
+-- Generation Time: Feb 10, 2020 at 04:25 PM
 -- Server version: 5.7.26
 -- PHP Version: 7.3.8
 
@@ -23,6 +23,263 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_country`
+--
+
+CREATE TABLE `tbl_country` (
+  `id` int(11) NOT NULL,
+  `country_code` varchar(2) NOT NULL DEFAULT '',
+  `country_name` varchar(100) NOT NULL DEFAULT ''
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `tbl_country`
+--
+
+INSERT INTO `tbl_country` (`id`, `country_code`, `country_name`) VALUES
+(1, 'US', 'United States'),
+(2, 'CA', 'Canada'),
+(3, 'AF', 'Afghanistan'),
+(4, 'AL', 'Albania'),
+(5, 'DZ', 'Algeria'),
+(6, 'DS', 'American Samoa'),
+(7, 'AD', 'Andorra'),
+(8, 'AO', 'Angola'),
+(9, 'AI', 'Anguilla'),
+(10, 'AQ', 'Antarctica'),
+(11, 'AG', 'Antigua and/or Barbuda'),
+(12, 'AR', 'Argentina'),
+(13, 'AM', 'Armenia'),
+(14, 'AW', 'Aruba'),
+(15, 'AU', 'Australia'),
+(16, 'AT', 'Austria'),
+(17, 'AZ', 'Azerbaijan'),
+(18, 'BS', 'Bahamas'),
+(19, 'BH', 'Bahrain'),
+(20, 'BD', 'Bangladesh'),
+(21, 'BB', 'Barbados'),
+(22, 'BY', 'Belarus'),
+(23, 'BE', 'Belgium'),
+(24, 'BZ', 'Belize'),
+(25, 'BJ', 'Benin'),
+(26, 'BM', 'Bermuda'),
+(27, 'BT', 'Bhutan'),
+(28, 'BO', 'Bolivia'),
+(29, 'BA', 'Bosnia and Herzegovina'),
+(30, 'BW', 'Botswana'),
+(31, 'BV', 'Bouvet Island'),
+(32, 'BR', 'Brazil'),
+(33, 'IO', 'British lndian Ocean Territory'),
+(34, 'BN', 'Brunei Darussalam'),
+(35, 'BG', 'Bulgaria'),
+(36, 'BF', 'Burkina Faso'),
+(37, 'BI', 'Burundi'),
+(38, 'KH', 'Cambodia'),
+(39, 'CM', 'Cameroon'),
+(40, 'CV', 'Cape Verde'),
+(41, 'KY', 'Cayman Islands'),
+(42, 'CF', 'Central African Republic'),
+(43, 'TD', 'Chad'),
+(44, 'CL', 'Chile'),
+(45, 'CN', 'China'),
+(46, 'CX', 'Christmas Island'),
+(47, 'CC', 'Cocos (Keeling) Islands'),
+(48, 'CO', 'Colombia'),
+(49, 'KM', 'Comoros'),
+(50, 'CG', 'Congo'),
+(51, 'CK', 'Cook Islands'),
+(52, 'CR', 'Costa Rica'),
+(53, 'HR', 'Croatia (Hrvatska)'),
+(54, 'CU', 'Cuba'),
+(55, 'CY', 'Cyprus'),
+(56, 'CZ', 'Czech Republic'),
+(57, 'DK', 'Denmark'),
+(58, 'DJ', 'Djibouti'),
+(59, 'DM', 'Dominica'),
+(60, 'DO', 'Dominican Republic'),
+(61, 'TP', 'East Timor'),
+(62, 'EC', 'Ecudaor'),
+(63, 'EG', 'Egypt'),
+(64, 'SV', 'El Salvador'),
+(65, 'GQ', 'Equatorial Guinea'),
+(66, 'ER', 'Eritrea'),
+(67, 'EE', 'Estonia'),
+(68, 'ET', 'Ethiopia'),
+(69, 'FK', 'Falkland Islands (Malvinas)'),
+(70, 'FO', 'Faroe Islands'),
+(71, 'FJ', 'Fiji'),
+(72, 'FI', 'Finland'),
+(73, 'FR', 'France'),
+(74, 'FX', 'France, Metropolitan'),
+(75, 'GF', 'French Guiana'),
+(76, 'PF', 'French Polynesia'),
+(77, 'TF', 'French Southern Territories'),
+(78, 'GA', 'Gabon'),
+(79, 'GM', 'Gambia'),
+(80, 'GE', 'Georgia'),
+(81, 'DE', 'Germany'),
+(82, 'GH', 'Ghana'),
+(83, 'GI', 'Gibraltar'),
+(84, 'GR', 'Greece'),
+(85, 'GL', 'Greenland'),
+(86, 'GD', 'Grenada'),
+(87, 'GP', 'Guadeloupe'),
+(88, 'GU', 'Guam'),
+(89, 'GT', 'Guatemala'),
+(90, 'GN', 'Guinea'),
+(91, 'GW', 'Guinea-Bissau'),
+(92, 'GY', 'Guyana'),
+(93, 'HT', 'Haiti'),
+(94, 'HM', 'Heard and Mc Donald Islands'),
+(95, 'HN', 'Honduras'),
+(96, 'HK', 'Hong Kong'),
+(97, 'HU', 'Hungary'),
+(98, 'IS', 'Iceland'),
+(99, 'IN', 'India'),
+(100, 'ID', 'Indonesia'),
+(101, 'IR', 'Iran (Islamic Republic of)'),
+(102, 'IQ', 'Iraq'),
+(103, 'IE', 'Ireland'),
+(104, 'IL', 'Israel'),
+(105, 'IT', 'Italy'),
+(106, 'CI', 'Ivory Coast'),
+(107, 'JM', 'Jamaica'),
+(108, 'JP', 'Japan'),
+(109, 'JO', 'Jordan'),
+(110, 'KZ', 'Kazakhstan'),
+(111, 'KE', 'Kenya'),
+(112, 'KI', 'Kiribati'),
+(113, 'KP', 'Korea, Democratic People\'s Republic of'),
+(114, 'KR', 'Korea, Republic of'),
+(115, 'KW', 'Kuwait'),
+(116, 'KG', 'Kyrgyzstan'),
+(117, 'LA', 'Lao People\'s Democratic Republic'),
+(118, 'LV', 'Latvia'),
+(119, 'LB', 'Lebanon'),
+(120, 'LS', 'Lesotho'),
+(121, 'LR', 'Liberia'),
+(122, 'LY', 'Libyan Arab Jamahiriya'),
+(123, 'LI', 'Liechtenstein'),
+(124, 'LT', 'Lithuania'),
+(125, 'LU', 'Luxembourg'),
+(126, 'MO', 'Macau'),
+(127, 'MK', 'Macedonia'),
+(128, 'MG', 'Madagascar'),
+(129, 'MW', 'Malawi'),
+(130, 'MY', 'Malaysia'),
+(131, 'MV', 'Maldives'),
+(132, 'ML', 'Mali'),
+(133, 'MT', 'Malta'),
+(134, 'MH', 'Marshall Islands'),
+(135, 'MQ', 'Martinique'),
+(136, 'MR', 'Mauritania'),
+(137, 'MU', 'Mauritius'),
+(138, 'TY', 'Mayotte'),
+(139, 'MX', 'Mexico'),
+(140, 'FM', 'Micronesia, Federated States of'),
+(141, 'MD', 'Moldova, Republic of'),
+(142, 'MC', 'Monaco'),
+(143, 'MN', 'Mongolia'),
+(144, 'MS', 'Montserrat'),
+(145, 'MA', 'Morocco'),
+(146, 'MZ', 'Mozambique'),
+(147, 'MM', 'Myanmar'),
+(148, 'NA', 'Namibia'),
+(149, 'NR', 'Nauru'),
+(150, 'NP', 'Nepal'),
+(151, 'NL', 'Netherlands'),
+(152, 'AN', 'Netherlands Antilles'),
+(153, 'NC', 'New Caledonia'),
+(154, 'NZ', 'New Zealand'),
+(155, 'NI', 'Nicaragua'),
+(156, 'NE', 'Niger'),
+(157, 'NG', 'Nigeria'),
+(158, 'NU', 'Niue'),
+(159, 'NF', 'Norfork Island'),
+(160, 'MP', 'Northern Mariana Islands'),
+(161, 'NO', 'Norway'),
+(162, 'OM', 'Oman'),
+(163, 'PK', 'Pakistan'),
+(164, 'PW', 'Palau'),
+(165, 'PA', 'Panama'),
+(166, 'PG', 'Papua New Guinea'),
+(167, 'PY', 'Paraguay'),
+(168, 'PE', 'Peru'),
+(169, 'PH', 'Philippines'),
+(170, 'PN', 'Pitcairn'),
+(171, 'PL', 'Poland'),
+(172, 'PT', 'Portugal'),
+(173, 'PR', 'Puerto Rico'),
+(174, 'QA', 'Qatar'),
+(175, 'RE', 'Reunion'),
+(176, 'RO', 'Romania'),
+(177, 'RU', 'Russian Federation'),
+(178, 'RW', 'Rwanda'),
+(179, 'KN', 'Saint Kitts and Nevis'),
+(180, 'LC', 'Saint Lucia'),
+(181, 'VC', 'Saint Vincent and the Grenadines'),
+(182, 'WS', 'Samoa'),
+(183, 'SM', 'San Marino'),
+(184, 'ST', 'Sao Tome and Principe'),
+(185, 'SA', 'Saudi Arabia'),
+(186, 'SN', 'Senegal'),
+(187, 'SC', 'Seychelles'),
+(188, 'SL', 'Sierra Leone'),
+(189, 'SG', 'Singapore'),
+(190, 'SK', 'Slovakia'),
+(191, 'SI', 'Slovenia'),
+(192, 'SB', 'Solomon Islands'),
+(193, 'SO', 'Somalia'),
+(194, 'ZA', 'South Africa'),
+(195, 'GS', 'South Georgia South Sandwich Islands'),
+(196, 'ES', 'Spain'),
+(197, 'LK', 'Sri Lanka'),
+(198, 'SH', 'St. Helena'),
+(199, 'PM', 'St. Pierre and Miquelon'),
+(200, 'SD', 'Sudan'),
+(201, 'SR', 'Suriname'),
+(202, 'SJ', 'Svalbarn and Jan Mayen Islands'),
+(203, 'SZ', 'Swaziland'),
+(204, 'SE', 'Sweden'),
+(205, 'CH', 'Switzerland'),
+(206, 'SY', 'Syrian Arab Republic'),
+(207, 'TW', 'Taiwan'),
+(208, 'TJ', 'Tajikistan'),
+(209, 'TZ', 'Tanzania, United Republic of'),
+(210, 'TH', 'Thailand'),
+(211, 'TG', 'Togo'),
+(212, 'TK', 'Tokelau'),
+(213, 'TO', 'Tonga'),
+(214, 'TT', 'Trinidad and Tobago'),
+(215, 'TN', 'Tunisia'),
+(216, 'TR', 'Turkey'),
+(217, 'TM', 'Turkmenistan'),
+(218, 'TC', 'Turks and Caicos Islands'),
+(219, 'TV', 'Tuvalu'),
+(220, 'UG', 'Uganda'),
+(221, 'UA', 'Ukraine'),
+(222, 'AE', 'United Arab Emirates'),
+(223, 'GB', 'United Kingdom'),
+(224, 'UM', 'United States minor outlying islands'),
+(225, 'UY', 'Uruguay'),
+(226, 'UZ', 'Uzbekistan'),
+(227, 'VU', 'Vanuatu'),
+(228, 'VA', 'Vatican City State'),
+(229, 'VE', 'Venezuela'),
+(230, 'VN', 'Vietnam'),
+(231, 'VG', 'Virigan Islands (British)'),
+(232, 'VI', 'Virgin Islands (U.S.)'),
+(233, 'WF', 'Wallis and Futuna Islands'),
+(234, 'EH', 'Western Sahara'),
+(235, 'YE', 'Yemen'),
+(236, 'YU', 'Yugoslavia'),
+(237, 'ZR', 'Zaire'),
+(238, 'ZM', 'Zambia'),
+(239, 'ZW', 'Zimbabwe');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_user`
 --
 
@@ -32,8 +289,8 @@ CREATE TABLE `tbl_user` (
   `lastname` varchar(50) NOT NULL,
   `email` varchar(70) NOT NULL,
   `country` varchar(100) NOT NULL,
-  `subscribe` date NOT NULL,
-  `lastupdate` date NOT NULL
+  `subscribe` datetime NOT NULL,
+  `lastupdate` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -41,11 +298,17 @@ CREATE TABLE `tbl_user` (
 --
 
 INSERT INTO `tbl_user` (`id`, `firstname`, `lastname`, `email`, `country`, `subscribe`, `lastupdate`) VALUES
-(1, 'testuser', 'aaa', 'test@user.ca', 'canada', '2020-02-10', '2020-02-10');
+(1, 'testuser', 'aaa', 'test@user.ca', 'canada', '2020-02-10 00:00:00', '2020-02-10 00:00:00');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `tbl_country`
+--
+ALTER TABLE `tbl_country`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tbl_user`
@@ -56,6 +319,12 @@ ALTER TABLE `tbl_user`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `tbl_country`
+--
+ALTER TABLE `tbl_country`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=240;
 
 --
 -- AUTO_INCREMENT for table `tbl_user`
